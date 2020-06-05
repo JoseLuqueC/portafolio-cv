c4b4cd25f6ff3603c48ee5901194cee664191b11
Jose Luque <joseluque93@hotmail.com>
2020-06-04 23:00:48 -0500

    routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+
+const app_routes: Routes = [
+  { path: '', component: PortafolioComponent},
+  { path: 'home', component: PortafolioComponent},
+  { path: 'projects', component: ProjectsComponent},
+  // { path: 'contact', component: ContactComponent},
+  { path: 'about', component: AboutComponent},
+  { path: '**', pathMatch: 'full', redirectTo: ''}
+];
+
+
+@NgModule({
+  imports:[
+    RouterModule.forRoot(app_routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,26 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidenavComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
     MatButtonModule,
