5818e62183cd3df96b1d561e01cd73a6738c1135
JOSE FERNANDO LUQUE <jsluque@xrm.com.co>
2021-02-01 10:21:22 -0500

    Firebase key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDqoRR3gl10M9wI55R-76iyilXzbLL9wAI",
+    authDomain: "joseluqueweb.firebaseapp.com",
+    databaseURL: "https://joseluqueweb.firebaseio.com",
+    projectId: "joseluqueweb",
+    storageBucket: "joseluqueweb.appspot.com",
+    messagingSenderId: "761560570995",
+    appId: "1:761560570995:web:995af795ebc4f597cd5e06",
+    measurementId: "G-FL1MSTZBE2"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDqoRR3gl10M9wI55R-76iyilXzbLL9wAI",
+    authDomain: "joseluqueweb.firebaseapp.com",
+    databaseURL: "https://joseluqueweb.firebaseio.com",
+    projectId: "joseluqueweb",
+    storageBucket: "joseluqueweb.appspot.com",
+    messagingSenderId: "761560570995",
+    appId: "1:761560570995:web:995af795ebc4f597cd5e06",
+    measurementId: "G-FL1MSTZBE2"
+  },
 };
 
 /*
